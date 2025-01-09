f609bace775be822c34023231428886d507ff031
devGatito <eliasmisaelsotojimenez1@gmail.com>
2025-01-09 13:10:06 -0500

    login with fireabse

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,16 +3,13 @@ import { Route } from '@angular/router';
 import { ResetPasswordComponent } from './components/reset-password/reset-password.component';  // Asegúrate de que esta ruta esté bien configurada
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
-import { RedComponent } from './components/red/red.component';
-import { CreatePostComponent } from './components/create-post/create-post.component';
 
 export const routes: Route[] = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'reset-password', component: ResetPasswordComponent }, 
   { path: 'login', component: LoginComponent },  
   { path: 'register', component: RegisterComponent },
-  {path: 'home', component: RedComponent},
-  {path: 'create-post', component: CreatePostComponent},
+ 
   
 
 ];
--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideAuth, getAuth } from '@angular/fire/auth';
-import { provideFirestore, getFirestore } from '@angular/fire/firestore'; // Importa el módulo de Firestore
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { environment } from './app/environments/environment';
 import { routes } from './app/app.routes';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
@@ -13,13 +13,12 @@ import { AuthInterceptor } from './app/interceptors/auth.interceptor';
 import { provideHttpClient } from '@angular/common/http';
 import { bootstrapApplication } from '@angular/platform-browser';
 
-// Configuración de Firebase y Firestore en la aplicación
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
     provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
     provideAuth(() => getAuth()),
-    provideFirestore(() => getFirestore()),  // Provee Firestore correctamente
+    provideFirestore(() => getFirestore()),  
     provideHttpClient(),
     {
       provide: HTTP_INTERCEPTORS,
